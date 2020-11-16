e9eb4734f2b54135e940fc8573637b0e8def6650
Isioma Chuck <isioma.chuck@gmail.com>
2020-11-16 19:45:48 +0000

    added nodemon 3

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-// const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
 // app.use(express.static(publicPath));
 
-
-
-app.use(express.static('client/build'))
+app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(publicPath, 'index.html'))
+        
+        // path.resolve(__dirname, 'client', 'build', 'index.html'))
 }) 
-
 
 const port = process.env.PORT || 5000
 
