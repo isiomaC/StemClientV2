ac301d7a3a59287e6f0911297b5ad2c0ed15eb1a
Isioma Chuck <isioma.chuck@gmail.com>
2020-11-16 19:06:42 +0000

    added more scripts to package.json

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+// const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.use(express.static(publicPath));
+
+if (process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    }) 
+}
+
+const port = process.env.PORT || 5000
+
 app.listen(port, () => {
-   console.log('Server is up!');
-});
+    console.log(`Server started on port ${port}`)
+})
