49b92537dcac4e8a6c81246c5bdaee446c377e96
Isioma Chuck <isioma.chuck@gmail.com>
2020-11-16 21:14:14 +0000

    committed

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
 })
 
-if (process.env.NODE_ENV === 'production'){ 
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    }) 
-}
+// if (process.env.NODE_ENV === 'production'){ 
+app.use(express.static('client/public'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+}) 
+// }
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
