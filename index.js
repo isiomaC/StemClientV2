ef8b2e07a12633dd919838559b03a21c966fa400
Isioma Chuck <isioma.chuck@gmail.com>
2020-11-16 19:17:14 +0000

    another try

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 // app.use(express.static(publicPath));
 
-if (process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    }) 
-}
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+}) 
+
 
 const port = process.env.PORT || 5000
 
