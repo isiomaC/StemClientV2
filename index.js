905bae3649492a1eb4d6b1d0aab4df40f2bf9d71
Isioma Chuck <isioma.chuck@gmail.com>
2020-11-16 20:29:45 +0000

    node mon 34

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-// const publicPath = path.join(__dirname, '..', 'public');
-// app.use(express.static(publicPath));
 
-app.use(express.static(path.join(__dirname, 'client/build')))
-
-app.get('*', (req, res) => {
-    // res.sendFile(path.join(publicPath, 'index.html'))
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-}) 
-
+if (process.env.NODE_ENV === 'production'){ 
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    }) 
+}
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
