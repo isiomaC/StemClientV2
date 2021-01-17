973c60758532a6b5b67006c383ec72b0b7c75f65
Isioma Chuck <isioma.chuck@gmail.com>
2021-01-17 06:08:35 +0000

    using client/public

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.use((err, req, res, next) => {
 
 // if (process.env.NODE_ENV === 'production'){ 
 // app.use(express.static('client/build'))
-app.use(express.static(path.join(__dirname, '/client/build')))
+app.use(express.static(path.join(__dirname, '/client/public')))
+console.log(express.static(path.join(__dirname, '/client/build')))
 app.get('*', (req, res) => {
+    console.log(path.resolve(__dirname, 'client', 'build', 'index.html'))
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     // res.sendFile(path.join(__dirname+'/client/build/index.html'));
 }) 
