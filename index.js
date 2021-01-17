98435dcc72f161c202010af436b6527d6ec15fb6
Isioma Chuck <isioma.chuck@gmail.com>
2021-01-17 06:00:50 +0000

    fixed path_join error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((err, req, res, next) => {
 
 // if (process.env.NODE_ENV === 'production'){ 
 // app.use(express.static('client/build'))
-app.use(express.static(path_join(__dirname, '/client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     // res.sendFile(path.join(__dirname+'/client/build/index.html'));
