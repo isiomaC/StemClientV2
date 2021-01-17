a41b076d6736a9f8a465cf9c2df4e3f57ec227ee
Isioma Chuck <isioma.chuck@gmail.com>
2021-01-17 06:28:01 +0000

    testing path.resolve for express.static

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ app.use((err, req, res, next) => {
 
 // if (process.env.NODE_ENV === 'production'){ 
 // app.use(express.static('client/build'))
-app.use(express.static(path.join(__dirname, '/client/public')))
-console.log(express.static(path.join(__dirname, '/client/build')))
+app.use(express.static(path.resolve(__dirname, 'client', 'build')))
 app.get('*', (req, res) => {
-    console.log(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    // console.log(express.static(path.resolve(__dirname, 'client', 'build')))
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     // res.sendFile(path.join(__dirname+'/client/build/index.html'));
 }) 
