8d6b65fd8672fa0e65c5590d65758c49a2cea364
Isioma Chuck <isioma.chuck@gmail.com>
2021-01-17 17:27:59 +0000

    added pre flight handling for options

--- index.js
+++ index.js
@@ -6,27 +6,13 @@ const app = express();
 require('dotenv').config()
 
 app.use(cors())
-
-// const allowedOrigins = [process.env.REACT_APP_CORS_LIST];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     // allow requests with no origin 
-//     // (like mobile apps or curl requests)
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    console.log(err)
+    console.log(err.message)
   
     // render the error page
     res.status(err.status || 500);
@@ -40,6 +26,10 @@ if (process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
         // res.sendFile(path.join(__dirname+'/client/build/index.html'));
     }) 
+    
+    app.options("*", (req, res) => {
+        res.status(200).send("Preflight request allowed");
+    });
 }
 
 const port = process.env.PORT || 5000
