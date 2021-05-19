b359ed549fd7c5d69355710a97b479264f66d8f8
Isioma Chuck <isioma.chuck@gmail.com>
2021-05-19 17:45:23 +0100

    Added express-sslify for https

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const enforce = require('express-sslify')
 
 require('dotenv').config()
 
@@ -23,18 +24,16 @@ if (process.env.NODE_ENV === 'production'){
     app.options("*", (req, res) => {
         res.status(200).send("Preflight request allowed");
     });
+    app.use(enforce.HTTPS({ trustProtoHeader: true }))
     app.use(express.static(path.resolve(__dirname, 'client', 'build')))
     app.get('*', (req, res) => {
         // console.log(express.static(path.resolve(__dirname, 'client', 'build')))
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
         // res.sendFile(path.join(__dirname+'/client/build/index.html'));
     }) 
-
-   
 }
 
 const port = process.env.PORT || 5000
-
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
